c914cc6d0f7bd35198a1d8bd3994493ebd7b8d5e
Salmeszpapi <74407068+Salmeszpapi@users.noreply.github.com>
2022-01-30 11:53:07 +0100

    Listing items in Market,need include user id button

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,9 +12,9 @@ namespace EnergyTrade.Controllers {
             if (!string.IsNullOrEmpty((string)Session["logged_in"])) {
                 return RedirectToAction("Index", "Market");
             }
-            //Session["logged_in"] = "kiki";
-            //Session["Logged_Id"] = 7;
-            //return RedirectToAction("Index", "Market");
+            Session["logged_in"] = "kiki";
+            Session["Logged_Id"] = 1;
+            return RedirectToAction("Index", "Market");
             return View("register");
 
             //return RedirectToAction("Index", "Market");
