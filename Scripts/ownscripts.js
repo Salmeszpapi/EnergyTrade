7270e416c4a0794de004a635115a28a7450ada9a
Salmeszpapi <74407068+Salmeszpapi@users.noreply.github.com>
2022-02-23 13:52:15 +0100

    Rollback

--- Scripts/ownscripts.js
+++ Scripts/ownscripts.js
@@ -1,1 +1,7 @@
-﻿
+﻿////var divs = document.querySelectorAll(".chat_list");    
+////divs.forEach((div) => {
+////    div.addEventListener('click', () => {
+////        console.log("forEach worked");
+////        div.style.backgroundColor = "green";
+////    })
+////});
